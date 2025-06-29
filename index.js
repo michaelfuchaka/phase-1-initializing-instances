90427bcb440d9059ff651acc9c36be3a371dfbf3
michaelfuchaka <fuchakamichael06@gmail.com>
2025-06-29 22:15:55 +0300

    Add classes for Breakfast, Lunch, and Dinner with constructor and private dessert

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code here
+// Write your code here// Breakfast class
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+// Lunch class
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+// Dinner class with private #dessert field
+class Dinner {
+  #dessert;
+
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#dessert = dessert;
+  }
+
+  // Optional: public method to access the private #dessert
+  getDessert() {
+    return this.#dessert;
+  }
+}
